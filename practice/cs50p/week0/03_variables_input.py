1a80338f1ab1fe4e58d4f47fe75f668922b0aa29
harshad-sasane <harshadsasane96@gmail.com>
2026-01-02 10:41:52 +0530

    practice: variables, user input, and pseudocode

---
+++ practice/cs50p/week0/03_variables_input.py
@@ -0,0 +1,21 @@
+"""
+Lecture 0: Variables, Comments, Pseudocode
+"""
+# Ask user for their name
+name = input("What's your name? ")
+
+# Comment explaining what we're doing
+# This demonstrates multiple print statements
+print("hello,")
+print(name)
+
+# Using pseudocode approach
+# TODO: Add age input
+# TODO: Combine name and age in greeting
+
+# Variables can change
+greeting = "Hello"
+print(greeting, name)
+
+# Try breaking it
+# print("Hello" + 123)  # What error do you get?

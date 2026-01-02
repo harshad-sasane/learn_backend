6f901d18afa26a8c90b21baf1bc82a6f92aed901
harshad-sasane <harshadsasane96@gmail.com>
2026-01-02 10:41:13 +0530

    practice: hello world program - basic print function

---
+++ practice/cs50p/week0/01_hello_world.py
@@ -0,0 +1,12 @@
+"""
+Lecture 0: Creating Code with Python
+Demonstrating the canonical first program
+"""
+print("hello, world")
+
+# First bug - what happens if we remove a parenthesis?
+# print("hello, world"
+
+# Experiment with different text
+print("Namaste, Duniya!")
+print("Hola Mundo!")
